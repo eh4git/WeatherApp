bddffa7c60b4e0fc6a8b812ed208d17d9fa0852c
erik hirsch <ehirsch760@gmail.com>
2020-05-05 09:56:10 -0700

    uv ajax working

--- script.js
+++ script.js
@@ -6,43 +6,56 @@ $(document).ready(function () {
 
         //get user input data
         const searchVal = $('#search-value').val();
-
-        //clear search input container
-        $('#search-value').empty();
-
+        
         //define apiKey
         const apiKey = `180c9f853ac8fcc595fe4080e0abf997`;
-
+        
         //construct a query url (template literal)
-        const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${searchVal}&appid=${apiKey}&units=imperial`;
+        const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchVal}&appid=${apiKey}&units=imperial`;
         $.ajax({
             url: queryURL,
             method: "GET",
             dataType: "json",
-            success: function(res) {
+            success: function (res) {
                 console.log(res);
                 //create elments and add desired res
-                const currentWeatherresHTML =
+                const currentWeatherHTML =
                 `
                 <h3 class="card-title">${res.name} ${new Date().toLocaleDateString()}</h3>
-                <div class="card">
+                    <div class="card">
                     <div class="card-body">
-                        <h3 class="card-title">${res.name} (${new Date().toLocaleDateString()})
-                            <img src="http://openweathermap.org/img/w/${res.weather[0].icon}.png">
-                        </h3>
-                        <p class="card-text">Temperature: ${res.main.temp} °F</p>
-                        <p class="card-text">Humidity: ${res.main.humidity}%</p>
-                        <p class="card-text">Wind Speed: ${res.wind.speed} MPH</p>
+                    <h3 class="card-title">${res.name} (${new Date().toLocaleDateString()})
+                    <img src="https://openweathermap.org/img/w/${res.weather[0].icon}.png">
+                    </h3>
+                    <p class="card-text">Temperature: ${res.main.temp} °F</p>
+                    <p class="card-text">Humidity: ${res.main.humidity}%</p>
+                    <p class="card-text">Wind Speed: ${res.wind.speed} MPH</p>
                     </div>
-                </div>
-                `;
-                $("#today").html(currentWeatherresHTML);
+                    </div>
+                    `;
+                    const recentCityButton = $("<li><button>" + res.name + "</button></li>");
+                    $(".history").prepend(recentCityButton).on("click", function () {
+                        $("#today").html(currentWeatherHTML);
+                    });
+                    $("#today").html(currentWeatherHTML);
+                    var latitude = res.coord.lat;
+                    var longitude = res.coord.lon;
+                    const queryUVIndex = `https://api.openweathermap.org/data/2.5/uvi/forecast?&appid=${apiKey}`
+                    $.ajax({
+                        url: queryUVIndex + "&lat=" + latitude + "&lon=" + longitude,
+                        method: "GET",
+                        dataType: "json",
+                        success: function (res) {
+                            $(`<p class="card-text">Wind Speed: ${latitude} MPH</p>`)
+                            console.log(res)
+                        }
+                    });
+                    //clear search input container
+                    $('#search-value').empty();
             }
-        })
-
+        });
+        
     });
-
-
 });
 
 //template literals
